cc5fa9dea9cd801cc82c3abd39b1371f1b572b9d
h-100 <hira.javaid008@gmail.com>
2024-02-16 16:27:39 +0500

    ADD: first commit of the game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Grid from './components/Grid/Grid.jsx'
+import './App.css'
+const App = () =>  (
+  <div className="App">
+
+    <Grid>
+
+    </Grid>
+  </div>
+  
+  );
+
+
+export default App;
